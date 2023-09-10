801035f6c77b71d5bdb3c33208f1afcc3440663c
Aras Güngöre <arasgungore@192.168.1.36>
2023-09-10 12:20:48 +0300

    Added UMAP for feature engineering and GridSearchCV for hyperparameter optimization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import umap
 import joblib
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import StackingClassifier
 from sklearn.metrics import log_loss
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from lightgbm import LGBMClassifier
 from tensorflow.keras.models import Sequential
@@ -62,28 +64,60 @@ model_filename = "stacking_model.pkl"   # Define the filename for saving the mod
 # y_train = to_categorical(y_train, num_classes=n_classes)
 # y_val = to_categorical(y_val, num_classes=n_classes)
 
+# Define the parameter grid for hyperparameter tuning
+params_grid_pipeline = {
+    "umap__n_components": [15, 25, 50],
+    "umap__n_neighbors": [5, 10, 20],
+    "umap__min_dist": [0.05, 0.1, 0.2],
+    'cnn__N': [32, 64, 128],
+    'cnn__epochs': [6, 10],
+    'cnn__batch_size': [32, 64],
+}
+
+# Create a KerasClassifier with UMAP-transformed features
+cnn_model = KerasClassifier(build_fn=create_cnn_model, verbose=0)
+pipeline = Pipeline([("umap", umap.UMAP()), ("cnn", cnn_model)])
+grid_search = GridSearchCV(estimator=pipeline, param_grid=params_grid_pipeline, cv=3, verbose=2, n_jobs=-1)
+
+# Fit the GridSearchCV to find the best hyperparameters
+grid_search.fit(X_train, y_train)
+
+# Access the best hyperparameters
+best_params = grid_search.best_params_
+
+# Apply UMAP to reduce dimensionality
+umap_model = umap.UMAP(n_components=best_params['umap__n_components'],
+                       n_neighbors=best_params['umap__n_neighbors'], min_dist=best_params['umap__min_dist'])
+X_train = umap_model.fit_transform(X_train)
+X_val = umap_model.transform(X_val)
+X_test = umap_model.transform(X_test)
+
+# Create the final CNN model with UMAP-transformed features
+final_model = create_cnn_model(best_params['cnn__N'], best_params['umap__n_components'], n_classes)
+
+# Train the final model on the UMAP-transformed data
+final_model.fit(X_train, y_train, epochs=best_params['cnn__epochs'], batch_size=best_params['cnn__batch_size'])
+
 # Create base models (CNNs) as scikit-learn estimators
-base_models = [
-    ("cnn1", KerasClassifier(build_fn=lambda: create_cnn_model(256, n_features, n_classes), epochs=15, batch_size=32))
-]
+# base_models = [
+#     ("cnn1", KerasClassifier(build_fn=lambda: create_cnn_model(best_params['N'], best_params['n_components'], n_classes),
+#                              epochs=best_params['epochs'], batch_size=best_params['batch_size']))
+# ]
 
 # Define the stacking ensemble model
-stacking_model = StackingClassifier(estimators=base_models, final_estimator=LGBMClassifier(n_estimators=30, n_jobs=-1, force_col_wise=True))
+# final_model = StackingClassifier(estimators=base_models, final_estimator=LGBMClassifier(n_estimators=30, n_jobs=-1, force_col_wise=True))
 
-# Fit the stacking model on the scaled training data
-stacking_model.fit(X_train, y_train)
+# Fit the optimized model on the scaled training data
+# final_model.fit(X_train, y_train)
 
-# Load the saved stacking model from a file
-# loaded_stacking_model = joblib.load(model_filename)
+# Save the optimized model to a file
+# joblib.dump(final_model, model_filename)
 
-# Save the trained stacking model to a file
-# joblib.dump(stacking_model, model_filename)
-
-# Make predictions on the scaled validation set
-val_predictions = stacking_model.predict(X_val)
+# Make predictions on the scaled validation set using the optimized model
+val_predictions = final_model.predict(X_val)
 
 # Calculate prediction probabilities for validation predictions
-val_proba = stacking_model.predict_proba(X_val)
+val_proba = final_model.predict_proba(X_val)
 
 # Clip predicted probabilities to avoid extremes of the log function
 val_proba = np.clip(val_proba, a_min=1e-15, a_max=1 - 1e-15)
@@ -92,11 +126,11 @@ val_proba = np.clip(val_proba, a_min=1e-15, a_max=1 - 1e-15)
 val_log_loss = log_loss(y_val, val_proba)
 print(f"Validation Log Loss: {val_log_loss:.4f}")
 
-# Make predictions on the scaled test data
-test_predictions = stacking_model.predict(X_test)
+# Make predictions on the scaled test data using the optimized model
+test_predictions = final_model.predict(X_test)
 
 # Calculate prediction probabilities for test predictions
-test_proba = stacking_model.predict_proba(X_test)
+test_proba = final_model.predict_proba(X_test)
 
 # Clip predicted probabilities to avoid extremes of the log function
 test_proba = np.clip(test_proba, a_min=1e-15, a_max=1 - 1e-15)
